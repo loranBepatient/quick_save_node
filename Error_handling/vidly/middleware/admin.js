2d3634b00b92c3be534235537880040720bd6f6c
loran <laurentm@me.com>
2018-05-01 09:22:42 +0200

    add new directory for error handling

---
+++ Error_handling/vidly/middleware/admin.js
@@ -0,0 +1,13 @@
+const jwt = require('jsonwebtoken')
+
+admin = (req, res, next) => { 
+    
+    if (!req.user.isAdmin) { 
+        return res.status(403).send('access denied')
+    }
+
+    next()
+}
+
+
+module.exports = admin;
---
+++ Error_handling/vidly/middleware/auth.js
@@ -0,0 +1,20 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+const auth = (req, res, next) => {
+    const token = req.header('x-auth-token'); 
+    if (!token) { 
+        return res.status(401).send('Access denied. No token provided');
+    }
+    try { 
+        const decoded = jwt.verify(token, config.get('jwtPrivateKey'));
+        req.user = decoded;
+
+        next();
+    } 
+    catch(err) { 
+        res.status(400).send('Invalid token');
+    }
+}
+
+module.exports = auth;
