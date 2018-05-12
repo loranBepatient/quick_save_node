cefacecb95e1867cc79833bb782c2b5be50fe6a1
1wQasdTeedFrsweXcs234saS56Scxs5423 <ankittanna@hotmail.com>
2018-05-12 12:51:42 +0200

    Add CORS Headers for my api

--- Error_handling/vidly/startup/routes.js
+++ Error_handling/vidly/startup/routes.js
@@ -13,7 +13,7 @@ module.exports = (app,express) => {
         app.use(morgan('tiny'));
         app.use(function (req, res, next) {
             res.header("Access-Control-Allow-Origin", "*");
-            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+            res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-auth-token");
             next();
         });
     }
